3633d07ed8372f2763dbb0f6d7599b1f9b6901c5
Carlos <cbeckhauser@gmail.com>
2022-11-08 13:44:06 -0300

    CenteredButton created and started Institutional section

--- src/components/CenteredButton.js
+++ src/components/CenteredButton.js
@@ -1,10 +1,15 @@
 import React from "react";
 import { Container, Row, Button } from "react-bootstrap";
+import "./CenteredButton.scss";
 
 const CenteredButton = ({ children }) => (
   <Container>
     <Row className="d-flex justify-content-center">
-      <Button className="my-5 px-5 py-3" variant="success" size="lg">
+      <Button
+        className="my-5 px-5 py-3 btn-centered"
+        variant="success"
+        size="lg"
+      >
         {children}
       </Button>
     </Row>
