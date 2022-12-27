cedb98de24cf9b9416ac5b37aeb7f0d48997fbb5
Carlos <cbeckhauser@gmail.com>
2022-12-26 21:53:37 -0300

    fixed column size of hero for xs and lg

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -7,11 +7,11 @@ import "./Hero.scss";
 const Hero = ({ onClick }) => (
   <Card className="text-center text-light hero">
     <Row className="my-lg-5">
-      <Col className="text-lg-end mt-5">
+      <Col xs={12} lg={6} className="text-lg-end mt-5">
         <Image className="d-lg-none" src={logo} />
         <Image className="d-none d-lg-inline-block" src={logoDesktop} />
       </Col>
-      <Col className="text-lg-start my-5">
+      <Col xs={12} lg={6} className="text-lg-start my-5">
         <p>Pague suas contas pelo nosso APP</p>
         <Button variant="outline-light" onClick={onClick}>
           Abra sua conta
